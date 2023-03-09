f9a68b59f9d11da8f4bb92db29adec4b219ea20d
Lem Euro <mr.lemeuro@gmail.com>
2023-03-09 14:19:41 +0000

    Finish app

--- ConvertMe/ContentView.swift
+++ ConvertMe/ContentView.swift
@@ -8,14 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var input = 100.0
+    @State private var selectedUnits = 0
+    @State private var inputUnit: Dimension = UnitTemperature.celsius
+    @State private var outputUnit: Dimension = UnitTemperature.fahrenheit
+    @FocusState private var showingKeyboard: Bool
+    
+    let conversions = ["Distance", "Mass", "Temperature", "Time"]
+    
+    let unitTypes = [
+        [UnitLength.feet, UnitLength.kilometers, UnitLength.meters, UnitLength.miles, UnitLength.yards],
+        [UnitMass.grams, UnitMass.kilograms, UnitMass.ounces, UnitMass.pounds],
+        [UnitTemperature.celsius, UnitTemperature.fahrenheit, UnitTemperature.kelvin],
+        [UnitDuration.hours, UnitDuration.minutes, UnitDuration.seconds]
+    ]
+    
+    let formatter: MeasurementFormatter
+    
+    var result: String {
+        let inputMeasurement = Measurement(value: input, unit: inputUnit)
+        let outputMeasurement = inputMeasurement.converted(to: outputUnit)
+        return formatter.string(from: outputMeasurement)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $input, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($showingKeyboard)
+                } header: {
+                    Text("Amount to convert")
+                }
+                
+                Picker("Conversion", selection: $selectedUnits) {
+                    ForEach(0..<conversions.count, id: \.self) {
+                        Text(conversions[$0])
+                    }
+                }
+                
+                Picker("Convert from", selection: $inputUnit) {
+                    ForEach(unitTypes[selectedUnits], id: \.self) {
+                        Text(formatter.string(from: $0).capitalized)
+                    }
+                }
+                
+                Picker("Convert to", selection: $outputUnit) {
+                    ForEach(unitTypes[selectedUnits], id: \.self) {
+                        Text(formatter.string(from: $0).capitalized)
+                    }
+                }
+                
+                Section {
+                    Text(result)
+                } header: {
+                    Text("Result")
+                }
+            }
+            .navigationTitle("ConvertMe")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        showingKeyboard = false
+                    }
+                }
+            }
+            .onChange(of: selectedUnits) { newSelection in
+                let units = unitTypes[newSelection]
+                inputUnit = units[0]
+                outputUnit = units[1]
+            }
         }
-        .padding()
+    }
+    
+    init() {
+        formatter = MeasurementFormatter()
+        formatter.unitOptions = .providedUnit
+        formatter.unitStyle = .long
     }
 }
 
