f12d3da7b4a9fede128da96581d0bd4dd88dcce6
Lem Euro <mr.lemeuro@gmail.com>
2023-03-09 12:17:42 +0000

    Initial Commit

---
+++ ConvertMe/ConvertMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertMeApp.swift
+//  ConvertMe
+//
+//  Created by Lem Euro on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConvertMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
